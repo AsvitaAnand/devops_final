e528460ad840012f982a23bf2e95470d5d103270
AsvitaAnand <127740114+AsvitaAnand@users.noreply.github.com>
2025-04-06 15:52:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker build -t task_final .
 echo Hyyy
-docker login -u boopeshs -p 02-Apr-05
-docker tag task_final boopeshs/devops_final
-docker push boopeshs/devops_final
+docker login -u asvita12 -p asvinu@12
+docker tag task_final asvita12/final_devops
+docker push asvita12/final_devops
 kubectl apply -f  deploy.yaml --validate=false
 kubectl apply -f svc.yaml --validate=false

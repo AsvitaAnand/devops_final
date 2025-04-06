f1970d2a3ae5a16e24e2dcb68611132bd84c67b8
AsvitaAnand <127740114+AsvitaAnand@users.noreply.github.com>
2025-04-06 15:58:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ echo Hyyy
 docker login -u asvita12 -p asvinu@12
 docker tag task_final asvita12/final_devops
 docker push asvita12/final_devops
-kubectl apply -f  deploy.yaml --validate=false
+minikube delete
+minikube start
+kubectl config use-context minikube
+kubectl a pply -f  deploy.yaml --validate=false
 kubectl apply -f svc.yaml --validate=false
